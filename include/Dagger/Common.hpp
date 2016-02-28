e4d5dc18652977788c83a7df1e8c26c898fe7322
Jesse Purcell <jpurcell656@gmail.com>
2016-02-28 22:15:45 +1000

    Initial Commit

---
+++ include/Dagger/Common.hpp
@@ -0,0 +1,10 @@
+#ifndef DAGGER_COMMON_HPP
+#define DAGGER_COMMON_HPP
+
+namespace Dagger
+{
+	typedef Float32 = float;
+	typedef Float64 = double;
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <Dagger\Common.hpp>
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+
+	return 0;
+}
